e0f5c93da5034c9813684b636cd46d3848570577
JDench <34353759+JDench@users.noreply.github.com>
2018-01-22 15:50:51 -0500

    more plot controls
    I've now added the ability to control more plotting parameters, particularly the cex arguments.

--- plot_completeScatter.v.1.r
+++ plot_completeScatter.v.1.r
@@ -7,10 +7,11 @@
 # This function depends on the define_plotSpace, definedJitter, and colTransparency functions I've written.
 # NOTE: This default plotting space I've worked with will look nice at height = 16, width =32, for 6 different subX types and 2 categoricalX
 plot_completeScatter <- function(func_inData, func_plotY = 1, func_plotX = 2, func_subX =3, func_IDcol = 4, 
-									func_plotJitter = TRUE, func_jitterRange = NULL, 
-									func_plotCI = TRUE, func_CIoppacity = 0.3,
+									func_plotJitter = TRUE, func_jitterRange = NULL, func_visualSeparators = FALSE,
+									func_plotCI = TRUE, func_CIoppacity = 0.3, func_sigFig = 3,
 									func_plot_mfcol = c(1,1), func_plotMar = c(10,10,5,2)+0.1, func_plotCols = NULL,
-									func_xLab = "X Types", func_xLas = 2, func_yLab = "Y Values", 
+									func_xLab = "X Types", func_xLas = 2, func_yLab = "Y Values",
+									func_cexAxis = 3, func_cexLab = 4, func_cexLegend = 2, func_cexPch = 3.5, 
 									func_categoricalOrder = NULL, func_subOrder = NULL,
 									func_plotMean = TRUE, func_addLine = NULL, func_yRange = NULL, 
 									func_legendSpot = "topright", func_legendArgs = NULL,
@@ -81,23 +82,35 @@ plot_completeScatter <- function(func_inData, func_plotY = 1, func_plotX = 2, fu
 									func_tmpBuffer_min = func_spaceParms[["bufferMin"]])
 	# Now if there has been now y-Range supplied, we'll use the data
 	if(is.null(func_yRange)){
-		func_yRange <- round(seq(min(func_inData[, func_plotY]),max(func_inData[, func_plotY]),length.out=7),3)
+		func_yRange <- round(seq(min(func_inData[, func_plotY]),max(func_inData[, func_plotY]),length.out=7), func_sigFig)
 	}
 	
 	# These are parameters for the plot
 	par(mfcol= func_plot_mfcol, mar = func_plotMar)
 	# Right now we can setup our blank plotting space
 	plot(x=NULL,y=NULL,xlab=NULL,xlim= tmp_xRange, ylab=NULL,ylim=c(min(func_yRange),max(func_yRange)),
-			axes=FALSE, ann = FALSE, bty="n", cex.lab=4)
+			axes=FALSE, ann = FALSE, bty="n", cex.lab= func_cexLab)
 	
 	# This is the x-lab and axis tickmarks
 	axis(side=1,padj=1.25,at= sapply(1:length(tmp_xAxis),function(x){ mean(unlist(tmp_xAxis[[x]])) }),
-			labels=as.character(all_subX), las= func_xLas,cex.axis=3)
-	mtext(func_xLab,side=1,line=8,outer=FALSE, cex = 4)
+			labels=as.character(all_subX), las= func_xLas,cex.axis= func_cexAxis)
+	mtext(func_xLab,side=1,line=8,outer=FALSE, cex = func_cexLab)
 	
 	# This is the y-lab and axis tick marks
-	axis(side=2,at=func_yRange, las=1,cex.axis=3)
-	mtext(func_yLab,side=2,line=7,outer=FALSE, cex = 4)
+	axis(side=2,at=func_yRange, las=1,cex.axis= func_cexAxis)
+	mtext(func_yLab,side=2,line=7,outer=FALSE, cex = func_cexLab)
+	
+	# If the user has requested that there be visual separators , we plot boxes around alternating subX ranges
+	# these will be done as light grey boxes, I'm not giving choices at the moment for customisation, ask if you care.
+	if(func_visualSeparators){
+		# We do alternating boxes around the even valued area
+		for(func_thisSeparator in seq(2,length(all_subX),by=2)){
+			polygon(x=c(rep(mean(c(max(unlist(tmp_xAxis[[func_thisSeparator-1]])),min(unlist(tmp_xAxis[[func_thisSeparator]])))),2),
+						rep(mean(c(max(unlist(tmp_xAxis[[func_thisSeparator]])),if(length(tmp_xAxis)> func_thisSeparator){min(unlist(tmp_xAxis[[func_thisSeparator + 1]]))}else{max(tmp_xRange)})),2)),
+					y=c(max(func_yRange),min(func_yRange),rev(c(max(func_yRange),min(func_yRange)))),
+					col= colTransparency("grey90",0.5), border = NA)	
+		}
+	}
 	
 	# If the user has request to plot an abline we pass their arguments assuming they've specified this properly.
 	if(!is.null(func_addLine)){
@@ -138,7 +151,7 @@ plot_completeScatter <- function(func_inData, func_plotY = 1, func_plotX = 2, fu
 					# Now we add points to out plot and colour each based on the mutant type
 					points(x=if(func_plotJitter){ definedJitter(length(func_subRows), if(is.null(func_jitterRange)){tmpPlot_xRange}else{func_jitterRange}) }else{ rep(tmpX,length(func_subRows)) },
 							y= func_inData[func_subRows, func_plotY],
-							col="black",pch=21,bg= tmp_colVec,cex=3.5)
+							col="black",pch=21,bg= tmp_colVec,cex= func_cexPch)
 				}
 			}
 		}
@@ -149,7 +162,7 @@ plot_completeScatter <- function(func_inData, func_plotY = 1, func_plotX = 2, fu
 		do.call(legend, func_legendArgs)
 	} else {
 		legend(func_legendSpot, legend= unlist(lapply(all_categoricalX,function(thisSet){ paste(thisSet, 1:length(func_plotCols[[thisSet]]),sep =" ") })),
-				fill= unlist(func_plotCols), bty="n",cex=2, ncol = length(all_categoricalX))
+				fill= unlist(func_plotCols), bty="n",cex= func_cexLegend, ncol = length(all_categoricalX))
 	}
 
 }
